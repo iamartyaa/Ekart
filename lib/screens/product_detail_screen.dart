bbbc211adbd157a368df8bc40f1b96642af308d1
Amartya Yadav <iamartyayadav@gmail.com>
2022-04-20 21:32:16 +0530

    used Provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
   static const routeName = '/product-detail';
+
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final products = Provider.of<Products>(context);
+    final product= products.items.firstWhere((element) => element.id==productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(product.title),
       ),
     );
   }
