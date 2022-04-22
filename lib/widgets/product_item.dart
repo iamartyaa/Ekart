17de448b66bb0694ac1543c78196c0b88b5039fa
Amartya Yadav <iamartyayadav@gmail.com>
2022-04-22 16:26:51 +0530

    Snackbars & Undoing

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,13 +4,11 @@ import 'package:shopapp/providers/product.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 
-
 class ProductItem extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-  final product = Provider.of<Product>(context);
-  final cart =  Provider.of<Cart>(context,listen: false);
+    final product = Provider.of<Product>(context);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -29,7 +27,9 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            onPressed: () {product.toggleFavourite();},
+            onPressed: () {
+              product.toggleFavourite();
+            },
             icon: Icon(
               product.isFavourite ? Icons.favorite : Icons.favorite_border,
             ),
@@ -41,7 +41,23 @@ class ProductItem extends StatelessWidget {
             style: const TextStyle(fontSize: 12),
           ),
           trailing: IconButton(
-            onPressed: () {cart.addItem(product.id, product.price, product.title);},
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to Cart',
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      }),
+                ),
+              );
+            },
             icon: const Icon(
               Icons.shopping_cart,
             ),
