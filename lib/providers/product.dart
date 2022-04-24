dd8fb7969695150238f27b67e88b576e3ca4eb9d
Amartya Yadav <iamartyayadav@gmail.com>
2022-04-25 03:19:09 +0530

    toggling favourites

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
-
   final String id;
   final String title;
   final String description;
@@ -14,12 +16,27 @@ class Product with ChangeNotifier {
     required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavourite=false,
+    this.isFavourite = false,
   });
 
-  void toggleFavourite(){
-    isFavourite = !isFavourite;
-    notifyListeners();
-  }
+  Future<void> toggleFavourite() async {
+    final oldStatus = isFavourite;
 
+    isFavourite = !isFavourite;
+
+    final url = Uri.https(
+        'shop-app-29cf9-default-rtdb.firebaseio.com', '/products/$id.json');
+    try
+    {
+      await http.patch(url,
+          body: jsonEncode({
+            'isFavourite': isFavourite,
+          }));
+      notifyListeners();
+    }catch(error){
+      isFavourite=oldStatus;
+      notifyListeners();
+      throw error;
+    }
+  }
 }
